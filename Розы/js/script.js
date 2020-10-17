8b889c920d30bdeeab137ce9cdaa8e9b4d7832b0
yagmurdzhi <73036376+yagmurdzhi@users.noreply.github.com>
2020-10-17 22:41:16 +0300

    Загрузка файлов сайта по продаже роз
    Загрузка файлов сайта по продаже роз

---
+++ Розы/js/script.js
@@ -0,0 +1,173 @@
+(function($) {
+    "use strict";
+
+    // Scroll to
+
+    $('a.scroll').smoothScroll({
+        speed: 800,
+        offset: -61
+    });
+
+    // Site navigation setup
+
+    var header = $('.header'),
+        pos = header.offset(),
+        blockTop = $('.block-top');
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > pos.top + 500 && header.hasClass('default')) {
+            header.fadeOut('fast', function() {
+                $(this).removeClass('default').addClass('switched-header').fadeIn(200);
+            });
+        } else if ($(this).scrollTop() <= pos.top + 500 && header.hasClass('switched-header')) {
+            header.fadeOut('fast', function() {
+                $(this).removeClass('switched-header').addClass('default').fadeIn(100);
+            });
+        }
+    });
+
+
+    //Hero resize
+
+    function mainHeroResize() {
+        $(" .hero .main-slider .slides li").css('height', $(window).height());
+    }
+
+    $(function() {
+            mainHeroResize()
+        }),
+        $(window).resize(function() {
+            mainHeroResize()
+        });
+
+
+    // Slider
+
+    $('.main-slider').flexslider({
+        animation: "fade",
+        slideshow: true,
+        directionNav: false,
+        controlNav: true,
+        pauseOnAction: false,
+        animationSpeed: 1000
+    });
+
+
+    $('.review-slider').flexslider({
+        animation: "slide",
+        slideshow: true,
+        directionNav: false,
+        controlNav: true,
+        pauseOnAction: false,
+        animationSpeed: 500
+    });
+
+
+    $('.offer-slider').flexslider({
+        animation: "slide",
+        slideshow: true,
+        directionNav: true,
+        controlNav: false,
+        pauseOnAction: false,
+        animationSpeed: 1000
+    });
+
+
+    // Mobile menu
+
+    var mobileBtn = $('.mobile-but');
+    var nav = $('.main-nav ul');
+    var navHeight = nav.height();
+
+    $(mobileBtn).on("click", function() {
+        $(".toggle-mobile-but").toggleClass("active");
+        nav.slideToggle();
+        $('.main-nav li a').addClass('mobile');
+        return false;
+
+
+    });
+
+    $(window).resize(function() {
+        var w = $(window).width();
+        if (w > 320 && nav.is(':hidden')) {
+            nav.removeAttr('style');
+            $('.main-nav li a').removeClass('mobile');
+        }
+
+    });
+
+    $('.main-nav li a').on("click", function() {
+        if ($(this).hasClass('mobile')) {
+            nav.slideToggle();
+            $(".toggle-mobile-but").toggleClass("active");
+        }
+
+    });
+
+
+
+    // Append images as css background
+
+    $('.background-img').each(function() {
+        var path = $(this).children('img').attr('src');
+        $(this).css('background-image', 'url("' + path + '")').css('background-position', 'initial');
+    });
+
+
+    // Tabbed content 
+
+    $(".block-tabs").on("click", "li", function() {
+        if (!$(this).hasClass("active")) {
+            var tabNum = $(this).index();
+            var nthChild = tabNum + 1;
+            $(".block-tabs li.active").removeClass("active");
+            $(this).addClass("active");
+            $(".block-tab li.active").removeClass("active");
+            $(".block-tab li:nth-child(" + nthChild + ")").addClass("active");
+
+        }
+    });
+
+
+
+
+    //Popup elements
+
+
+    $('.popup-image').magnificPopup({
+        type: 'image',
+        fixedContentPos: false,
+        fixedBgPos: false,
+        mainClass: 'mfp-no-margins mfp-with-zoom',
+        image: {
+            verticalFit: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300
+        }
+    });
+
+
+    $('.popup-youtube, .popup-vimeo').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: false
+    });
+
+
+    //Masonry gallery
+
+
+    var $container = $('.block-masonry');
+    $container.masonry({
+        itemSelector: '.block-item-masonry'
+    });
+
+
+})(jQuery);
---
+++ Розы/send.php
@@ -0,0 +1,19 @@
+<?php
+//Принимаем постовые данные
+$coments=$_POST['coments'];
+$name=$_POST['name'];
+$phone=$_POST['phone'];
+
+//Тут указываем на какой ящик посылать письмо
+$to = "yummyroses.kiev@gmail.com";
+//Далее идет тема и само сообщение
+$subject = "Заявка с сайта";
+$message = "Письмо отправлено из моей формы<br />
+Коментарий: ".htmlspecialchars($coments)."<br />
+Имя: ".htmlspecialchars($name)."<br />
+Телефон: ".htmlspecialchars($phone);
+$headers = "From: index.html <no-reply@mysite.ru>\r\nContent-type: text/html; charset=utf-8 \r\n";
+mail ($to, $subject, $message);
+header('Location:thanks.html');
+exit;
+?>
